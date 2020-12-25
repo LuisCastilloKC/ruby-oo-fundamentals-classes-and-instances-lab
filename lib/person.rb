4b9513a1da344b03413c51fa0a329cecdb541083
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2020-12-25 14:26:52 -0500

    commit tree 1d76667eb63882c3e316e542e3270314c4461460

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+
+# Person
+#   is defined within lib/person.rb
+#   variables defined in lib/person.rb
+#     defines a local variable adele_goldberg as an instance of a Person
+#     defines a local variable alan_kay as an instance of a Person
+
+# Finished in 0.00349 seconds (files took 0.0962 seconds to load)
+# 7 examples, 0 failures
